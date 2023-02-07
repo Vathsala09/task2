cebbe84a046c321014a6af26e1315ed9913e302d
vathsala p m <v.pradhaan.ce@gmail.com>
2023-02-07 18:42:56 +0530

    commit

---
+++ controller/user.js
@@ -0,0 +1,25 @@
+const User = require('../models/users');
+function isstringinvalid(string){
+  if(string == undefined ||string.length === 0){
+      return true
+  } else {
+      return false
+  }
+}
+
+const signup = async (req, res)=>{
+  try{
+  const { name, email, password } = req.body;
+  console.log('email', email)
+  if(isstringinvalid(name) || isstringinvalid(email || isstringinvalid(password))){
+      return res.status(400).json({err: "Bad parameters . Something is missing"})
+  }
+
+  
+      await User.create({ name, email, password})
+      res.status(201).json({message: 'Successfuly create new user'})
+}
+  catch(err) {
+          res.status(500).json(err);
+      }
+    }
